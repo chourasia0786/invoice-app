ca0ad24a94663bbf5c810eb2c1b9697b871d7b74
chourasia0786 <ashishkumar-mangaldas.chourasia@hpe.com>
2025-04-18 19:34:59 +0530

    Index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
-const isProd = process.env.NODE_ENV === 'production';
-
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={isProd ? "/invoice-app" : ""}>
+   <HashRouter basename="/invoice-app">
       <App />
-    </HashRouter>
+  </HashRouter>
+
   </React.StrictMode>
 );
